3e8ec127365f75a2d0118bf7337c25fdb2182482
Your Name <you@example.com>
2024-11-02 14:36:21 +0300

    cmt

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -25,8 +25,8 @@ export default async function handleToken(
       return;
     }
 
-    const roomName = `room-1111-aaaa`;
-    //const roomName = `room-${generateRandomAlphanumeric(4)}-${generateRandomAlphanumeric(4)}`;
+    //const roomName = `room-1111-aaaa`;
+    const roomName = `room-${generateRandomAlphanumeric(4)}-${generateRandomAlphanumeric(4)}`;
     const identity = `identity-${generateRandomAlphanumeric(4)}`
 
     const grant: VideoGrant = {
